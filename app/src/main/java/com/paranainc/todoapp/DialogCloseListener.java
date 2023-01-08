a5151506730125717b0880c70081a218304c9e76
Erick Azarkman <erick@azarkman.com>
2023-01-07 20:09:41 -0800

    First commit

---
+++ app/src/main/java/com/paranainc/todoapp/DialogCloseListener.java
@@ -0,0 +1,10 @@
+package com.paranainc.todoapp;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+
+    public void handleDialogClose(DialogInterface dialog);
+
+
+}
